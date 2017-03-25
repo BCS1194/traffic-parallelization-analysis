3626aec97c221c2c930cd2923cfd89c63e56aebd
Brundage <brundacs@student.cs.jmu.edu>
2017-03-25 18:02:37 -0400

    fixed car path type. added linked list struct to represent a path

--- road_test.c
+++ road_test.c
@@ -3,10 +3,16 @@
 #include <string.h>
 #include <time.h>
 
+//linked list
+/*typedef*/ struct node_log {
+  char node;        // The value or data stored in the node
+  struct node_log * next;  // A reference to the next node, null for last node
+} /*node_t*/;
+
 struct car {
   //int speed;
-  int size;     //size of the car; standard is 1
-  char path[];  //a record of the nodes visited by the car
+  int size;         //size of the car; standard is 1
+  struct node_log path;    //a record of the nodes visited by the car
 };
 
 struct road {
@@ -16,33 +22,33 @@ struct road {
   char origin;        //the start node
   char destination;   //the end node, must match the start node of the next road to be joined
   struct car cars[];  //a list of the cars currently on this road
-  
+
   //int length;       //the distance (in measurable units) of the road
   //int time;         //the time it would take for a car to get from start to end
 };
 
-
-int simulation_one() 
+int simulation_one()
 {
-  struct road a = {a.size, 0, 10};//, 'c', 'd', NULL};
+  struct road a = {a.size, 0, 10}; //, 'c', 'd', NULL};
+    //a.cars = [10];
+
   /*
     a.size        = 0;
     a.capacity    = 10;
-    a.origin	    = NULL;
+    a.origin      = NULL;
     a.destination = NULL;
     a.cost        = a.size;
   */
-  
+
     //a.length 	= 6;
     //a.time	= 1;
 
-  struct car car1 = {1, NULL};
+  struct car car1 = {1};
+
   /*
     car1.size   = 1;
     car1.path   = [];
   */
-  
-  
 }
 
 int main() 
