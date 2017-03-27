263658b8baccdda521a0853bcda6f686f6c256ae
Corey Brundage <brundacs@dukes.jmu.edu>
2017-03-26 21:59:37 -0400

    Delete Main.jl

--- Main.jl
+++
@@ -1,9 +0,0 @@
-# Main.jl
-include("Roads.jl")
-include("Cars.jl")
-
-using Roads, Cars
-
-roads = initRoads()
-car = initCar()
-println("$(car.travelWeight) is the travel weight and Road 12 ends at $(roads[12].destPosition)")
