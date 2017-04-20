b6eed7b6550191cca8c0b4876e8b765e7c5c4204
Jasmyn Kelly <kelly2jk@dukes.jmu.edu>
2017-04-20 16:01:02 -0400

    added some comments and cleaned up code

--- test_user_file_io_cars_roads.c
+++ test_user_file_io_cars_roads.c
@@ -3,6 +3,9 @@
 road_t shortest;
 int cars_buffer[100];
 
+/********************************************************************
+ * This is the driver function for the program.
+ *******************************************************************/
 int main()
 {
     int numLines;
@@ -16,11 +19,10 @@ int main()
     road_t roads[numLines];
     readFile(filename, roads, numLines);
     num_roads_remaining = (sizeof(roads) / sizeof(int)) / 2;
-    //printf("filename: %s\n", filename);
 
     printf("Enter a number (1-100) for the number of cars you would like to generate: ");
     scanf("%d", &cars_count);
-    printf("Your number is %d\n", cars_count);
+    printf("The number of cars on the road is %d\n", cars_count);
 
     for(int i = 0; i < cars_count; i++)
     {
@@ -32,25 +34,28 @@ int main()
         printf("%d\n", cars_buffer[i]);
     }*/
 
-   
-    printf("*****%d\n", numLines);
     simulation_one(roads, num_roads_remaining);
-
 
     //maybe implement another loop here to initialize all 100 entries to 0 first
 }
 
+/********************************************************************
+ * This function gets the name of the file.
+ *******************************************************************/
 void getFilename(char *filename) 
 {
     printf("Enter filename: ");
     scanf("%s", filename);
 }
 
+/********************************************************************
+ * This function reads the contents of a file and saves it to the
+ * road_t struct.
+ *******************************************************************/
 void readFile(char *filename, road_t roads[], int numLines)
 { 
     FILE * fp;
     int ii = 0;
-    int jj = 0;
 
     fp = fopen(filename, "r");//change to filename
     if (!fp) {
@@ -60,39 +65,34 @@ void readFile(char *filename, road_t roads[], int numLines)
     for(ii = 0; ii < numLines; ii++)
     {
       fscanf(fp,"%d,%c\n", &roads[ii].cost, &roads[ii].id);
-      printf("%d %c\n", roads[ii].cost, roads[ii].id);
+      //printf("%d %c\n", roads[ii].cost, roads[ii].id);
     }
 
     fclose(fp);
 }
 
+/********************************************************************
+ * Simulation.
+ *******************************************************************/
 void simulation_one(road_t roads[], int num_roads_remaining)
 {
     int jj = 0;
-    //num_roads_remaining = (sizeof(roads) / sizeof(int)) / 2;
-/*
-    road_t a = {roads[0].cost, roads[0].id}; // Origin road
-    road_t b = {roads[1].cost, roads[1].id}; // Origin road
-    road_t c = {roads[2].cost, roads[2].id};
-    road_t d = {roads[3].cost, roads[3].id};
-    road_t e = {roads[4].cost, roads[4].id};
-    road_t f = {roads[5].cost, roads[5].id}; // Origin road
-    road_t g = {roads[6].cost, roads[6].id}; // Origin road
-    road_t h = {roads[7].cost, roads[7].id};
-    road_t i = {roads[8].cost, roads[8].id};
-    road_t j = {roads[9].cost, roads[9].id};
-*/
 
     //printf("\n%ld\n", (sizeof(roads) / sizeof(int)) / 2);
-    printf("%d\n", num_roads_remaining);
+    //printf("%d\n", num_roads_remaining);
 
     printf("OUTSIDE CALL: %d %c\n", shortest.cost, shortest.id);
 
     shortest_path(roads, roads[0], roads[1], num_roads_remaining, jj);
 
-    printf("\n%d\n", shortest.cost);
+    printf("\nThe shortest cost road is %c with a cost of %d\n", shortest.id, shortest.cost);
 }
 
+/********************************************************************
+ * This function takes in different roads and will choose the
+ * cars next road based on the cost of traveling on each road, then
+ * it will return the road with the lowest cost/shortest path.
+ *******************************************************************/
 void shortest_path(road_t roads[], road_t a, road_t b, int num_roads_remaining, int jj)
 {
     printf("\nThis is call %d\n", jj + 1);
@@ -116,11 +116,15 @@ void shortest_path(road_t roads[], road_t a, road_t b, int num_roads_remaining, 
         shortest_path(roads, shortest, roads[jj], num_roads_remaining, jj);
 }
 
+/********************************************************************
+ * This function takes in a file and counts the number of lines
+ * that are used in the file and returns the number of lines.
+ *******************************************************************/
 int countLines(char *filename)
 {
     // count the number of lines in the file called filename
-    int ch=0;
-    int numLines=0;
+    int line = 0;
+    int numLines = 0;
     FILE *fp = fopen(filename,"r");
     
     
@@ -128,9 +132,9 @@ int countLines(char *filename)
         return 0;
     
     numLines++;
-    while ((ch = fgetc(fp)) != EOF)
+    while ((line = fgetc(fp)) != EOF)
     {
-        if (ch == '\n')
+        if (line == '\n')
             numLines++;
     }
     fclose(fp);
